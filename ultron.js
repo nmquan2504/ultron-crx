341e2dc9218f79c7cdca8e60b932bd18ffff7e6c
Quan Nguyen <qnguyen32@csc.com>
2017-02-08 11:26:17 +0700

    Update ultron.js

--- ultron.js
+++ ultron.js
@@ -1,4 +1,6 @@
 var ULTRON = ULTRON || new (function(_) {
+	
+	_.CurrentPath = "";
 
 	_.injectScript = function(url) {
 		try {
@@ -12,7 +14,40 @@ var ULTRON = ULTRON || new (function(_) {
 		}
 	};
 	
+	_.delayCall = function(func, time) {
+		var t = setInterval(function(){
+			func();
+			clearInterval(t);
+		}, time);
+		return 0;
+	}
+	
+	_.getCurrentPath = function() {
+		var $scripts = $("script[src*='ultron.js']");
+		if ($scripts.length > 0) {
+			_.CurrentPath = $scripts[0].src.split("/").slice(0, -1).join("/") + "/";
+		}
+		return _.CurrentPath;
+	}
+	
 	this.init = function() {
+		try {
+			if (!$) {
+				return _.delayCall(ULTRON.init);
+			}
+			
+			if (_.getCurrentPath()) {
+				if (location) {
+					if (location.host && location.host.indexOf("inboxdollars.com") >= 0) {
+						_.injectScript(_.CurrentPath + "inboxdollars/inboxdollars.js");
+					}
+				}
+			}
+		}
+		catch (error) {
+		}	
 	};
 
 })({});
+
+ULTRON.init();
